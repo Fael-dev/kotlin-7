adac8298f15124068c99d82ce7e09440ccb316a0
Rafael Leonan <faelleonan@gmail.com>
2023-07-01 15:26:31 -0300

    Função run

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,9 +1,40 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaPoupanca
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
+}
 
+fun testaRun() {
+    val taxaAnual = 0.05
+    val taxaMensal = taxaAnual / 12
+    println("Taxa mensal: $taxaMensal")
+
+
+    val contaPoupanca = ContaPoupanca(Cliente(
+        nome = "Rafael Rodrigues",
+        cpf = "111.111.111-11",
+        senha = 1234
+    ), 1000)
+
+    contaPoupanca.run {
+        deposita(1000.0)
+        saldo * taxaMensal
+    }.let{ rendimentoMensal ->
+        println("Rendimento mensal: $rendimentoMensal")
+    }
+
+    val rendimentoAnual = run {
+        var saldo = contaPoupanca.saldo
+        repeat(12) {
+            saldo += saldo * taxaMensal
+        }
+        saldo
+    }
+
+    println("Simulação rendimento anual: $rendimentoAnual")
 }
 
 fun testaWith() {
