325aebb8a4b6e0ad9912452d055e43417fc059f4
Rafael Leonan <faelleonan@gmail.com>
2023-07-01 15:36:58 -0300

    Implementando HOF com Receivers

--- src/br/com/alura/bytebank/teste/testaHOF.kt
+++ src/br/com/alura/bytebank/teste/testaHOF.kt
@@ -3,23 +3,26 @@ package br.com.alura.bytebank.teste
 import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.SistemaInterno
 
-fun soma(a: Int, b: Int, resultado: (Int) -> Unit) {
-    println("Antes da soma")
-    resultado(a + b)
-    println("Depois da soma")
-}
-
 fun testaHOF() { // HOF => Higher order function
-    soma(1, 6, resultado = (::println))
+    soma(1, 6, resultado = { valor ->
+        println(valor)
+    })
+    somaReceiver(1, 6, resultado = {
+        println(this)
+    })
 
     val autenticavel = object: Autenticavel {
         val senha = 1234
         override fun autentica(senha: Int) = this.senha == senha
     }
+    val sistema = SistemaInterno()
 
-    SistemaInterno().entra(autenticavel, 1234) { println("Realizar pagamento") }
+    sistema.entra(autenticavel, 1234) { println("Realizar pagamento") }
     // OU
     // SistemaInterno().entra(autenticavel, 1234, autenticado = { println("Realizar pagamento") })
+    sistema.entraReceiver(autenticavel, 1234, autenticado = {
+        pagamento()
+    })
 }
 
 fun testeHigherOrderFunction() {
@@ -34,3 +37,16 @@ fun testeRecebeString(valor: String) {}
 
 // Higher order function => Função que recebe ou devolve outra função
 fun teste(teste: Int, bloco: () -> Unit) {}
+
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit) {
+    println("Antes da soma")
+    resultado(a + b)
+    println("Depois da soma")
+}
+
+fun somaReceiver(a: Int, b: Int, resultado: Int.() -> Unit) {
+    println("Antes da soma")
+    val total = a + b
+    total.resultado()
+    println("Depois da soma")
+}
