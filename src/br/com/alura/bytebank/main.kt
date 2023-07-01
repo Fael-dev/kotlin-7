b1aed108bb9463ac1ffd2ee8627215c548219134
Rafael Leonan <faelleonan@gmail.com>
2023-07-01 13:22:05 -0300

    Funções lambda e anônima

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,15 +1,34 @@
 package br.com.alura.bytebank
 
 fun main() {
+    testaTipoFuncaoReferencia()
+    testaTipoFuncaoClasse()
+    val minhaFuncaoLambda: () -> Unit = {
+        println("Executa como lambda")
+    }
+    println(minhaFuncaoLambda)
+    println(minhaFuncaoLambda())
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Executa como lambda")
+    }
+    println(minhaFuncaoAnonima)
+    println(minhaFuncaoAnonima())
+}
+
+fun testaTipoFuncaoReferencia() {
     val minhaFuncao: () -> Unit = ::teste // Ou val minhaFuncao = ::teste
-    // println(minhaFuncao()) => não executa a função
-    val minhaFuncaoClasses: () -> Unit = Teste() // Ou val minhaFuncaoClasses = Teste()
-    // println(minhaFuncaoClasses()) => não executa a função
-    println(minhaFuncaoClasses())
+    // println(minhaFuncao) => não executa a função
+    println(minhaFuncao())
 }
 
 fun teste() {
     println("Executa função")
+}
+
+fun testaTipoFuncaoClasse() {
+    val minhaFuncaoClasses: () -> Unit = Teste() // Ou val minhaFuncaoClasses = Teste()
+    // println(minhaFuncaoClasses()) => não executa a função
+    println(minhaFuncaoClasses())
 }
 
 class Teste: () -> Unit {
