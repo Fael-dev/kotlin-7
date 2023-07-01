4e339455670db11873c1dd87fd426281f0db9785
Rafael Leonan <faelleonan@gmail.com>
2023-07-01 15:15:27 -0300

    Função with

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,5 +1,20 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.modelo.Endereco
+
 fun main() {
 
+}
+
+fun testaWith() {
+    with(Endereco()) {
+        logradouro = "Rua Regina Estela Cavalcante"
+        numero = 207
+        bairro = "Vila Velha"
+        cidade = "Fortaleza"
+        estado = "CE"
+        cep = "60345-827"
+        complemento = "casa"
+        completo()
+    }.let(::println)
 }
